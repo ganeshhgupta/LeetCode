0e3761bcff3d5f813a78cf1bb584da0967eee651
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-03-12 11:53:18 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.23%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -9,6 +9,8 @@ class Solution:
         if n == 2:
             return max(nums[0], nums[1])
 
+        #Recurrecnce relation: Max loot at i = max( Max loot till i-2 + Loot at i , Max loot till i-1)
+
         dp = [0]*n 
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
