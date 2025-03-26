0bbde59d593777954a60a72159a7ec0e51db167d
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-03-26 12:40:41 -0500

    Time: 4 ms (41.96%) | Memory: 17.7 MB (97.91%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -3,14 +3,18 @@ class Solution:
         
         n = len(s)
         dp_cahce = [False] * (n + 1)
+        #create a dp cache for the entire length of s = [False, False, False .... n times]
         dp_cahce[n] = True
+        #set last index to be True, we assume the end of string s is a break
 
         for i in range(n - 1, -1, -1):
             for w in wordDict:
                 w_length = len(w)
+                #for eg. s = "leetcode", the code will only enter this condition at i=4 'code' and i=0 'leet'
                 if i + w_length <= n and s[i:i+w_length] == w:
                     dp_cahce[i] = dp_cahce[i + w_length]
                 if dp_cahce[i]:
+                    #as soon as we find a word match, no need to check rest of the dict
                     break
-
+        #if we can successfully break s, dp[0] has to be True
         return dp_cahce[0]
