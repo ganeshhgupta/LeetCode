037f3acb7d48d8bd4f27845d9fe44f7ddcbf2346
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-03-27 12:35:36 -0500

    Time: 5 ms (11.09%) | Memory: 20 MB (5.71%) - LeetSync

---
+++ 57-insert-interval/insert-interval.py
@@ -0,0 +1,42 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+        if not intervals:
+            return [newInterval]
+
+        if newInterval[1] <  intervals[0][0]:
+            intervals.insert(0, newInterval) 
+            print("here")
+            return intervals
+
+        if newInterval[0] >  intervals[len(intervals) - 1][1]:
+            intervals.append(newInterval) 
+            print("no, here")
+            return intervals
+
+        for i in range(len(intervals)):
+            if intervals[i][1] >= newInterval[0]:
+                start = min(intervals[i][0], newInterval[0])
+                start_index = i
+                print("start: ", start, start_index)
+                break
+
+        for i in range(len(intervals) - 1, -1, -1):
+            if intervals[i][0] <= newInterval[1]:
+                end = max(intervals[i][1], newInterval[1])
+                end_index = i
+                print("end: ", end, end_index)
+                break
+
+        print(start, end)
+                
+        n = len(intervals) - 1
+        print(n )
+        print(start_index, end_index + 1)
+        for i in range(start_index, end_index + 1) :
+            intervals.pop(start_index)
+        print(intervals)
+        intervals.insert(start_index, [start, end])    
+
+        print(intervals)
+        return intervals
