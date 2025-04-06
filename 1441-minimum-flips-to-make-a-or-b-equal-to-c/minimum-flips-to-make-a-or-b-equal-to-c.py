c6e9c22fa7bf4697ab6885be3133ed37e1f28729
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-04-06 17:34:00 -0500

    Time: 40 ms (42.59%) | Memory: 17.6 MB (75.13%) - LeetSync

---
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        
+        a = list(bin(a))[2:]
+        b = list(bin(b))[2:]
+        c = list(bin(c))[2:]
+
+        max_len = max(len(a), len(b), len(c))
+
+        count = 0
+
+        for i in range(max_len - len(a)):
+            a.insert(0, '0')
+        for i in range(max_len - len(b)):
+            b.insert(0, '0')
+        for i in range(max_len - len(c)):
+            c.insert(0, '0')
+
+        for i in range(len(c)):
+            if int(a[i]) | int(b[i]) !=  int(c[i]):
+                count += abs(int(a[i]) + int(b[i]) - int(c[i]))
+                print(a[i], b[i], c[i], abs(int(a[i]) + int(b[i]) - int(c[i])))
+            
+        print(a)
+        print(b)
+        print(c)
+
+        print(count)
+        return count
