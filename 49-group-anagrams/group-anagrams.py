2077e22f3be33b8f5c70c636e38800faf0318af8
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-04-12 15:31:11 -0500

    Time: 15 ms (50.02%) | Memory: 22.9 MB (5.43%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,28 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        res = defaultdict(list) #map where key is int list, val is string list
+        res = defaultdict(list)
 
-        for s in strs: # for every word in the input string list
+        for str in strs:
 
-            count = [0] * 26 #creating an int list of 26 zeros
-
-            #00000000000000...
-
-            for c in s: #for every char in the word, flip corr int list 0 to 1
-                count[ord(c) - ord("a")] += 1
-
-                '''
-                for 'eat'
-                index of the respective alphabet in count[] array:
-                10001000..
-                abcdef...
-                '''
-
-            res[tuple(count)].append(s)
-
-            #key: 10001000 .. : will contain 'eat', 'ate', etc all the anangrams
-
-        #print(res)
-        #print(res.values())
+            count = [0] * 26
+            for c in str:
+                count[ord(c) - ord('a')] += 1
+            
+            res[tuple(count)].append(str)
+        
         return list(res.values())
