5091997e1f1e79aabc131fce1f02fbe74c65111d
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-04-15 18:19:54 -0500

    Time: 4 ms (3.50%) | Memory: 17.9 MB (38.24%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -8,29 +8,20 @@ class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         
         res = []
-
-        q = deque() #double ended queue
-        q.append(root) #append first level
-
+        q = collections.deque()
+        q.append(root)
+        
         while q:
-            q_len = len(q) #no. of elements in q at any given level
-            level = [] #to store all children from next level
-
-            for i in range(q_len):
-
-                node = q.popleft() #note popleft
-
-                #for every popped node at a level, append its children in q
-                #q_len with take care we never traverse more than len of prev level
+            l = len(q)
+            level = []
+            for i in range(l):
+                node = q.popleft()
                 if node:
                     level.append(node.val)
                     q.append(node.left)
                     q.append(node.right)
-                    
-            # after every iteration, append level list to main list
-            if level:
 
-                #only diff be level order trav and RHS view
+            if level:
                 res.append(level[-1])
 
         return res
