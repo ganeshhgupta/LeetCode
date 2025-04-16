a9fb829afe24800e196e7873c63a049c56853276
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-04-15 20:57:49 -0500

    Time: 31 ms (5.28%) | Memory: 21 MB (43.68%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -2,16 +2,15 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         
         i = 0
-        sorted_intervals = sorted(intervals, key=lambda x: x[0])
-        while i <= len(sorted_intervals) - 2:
-            #print(i)
-            if sorted_intervals[i][1] >= sorted_intervals[i+1][0] :
-                sorted_intervals.insert(i, [min(sorted_intervals[i][0],sorted_intervals[i+1][0]) , max(sorted_intervals[i+1][1], sorted_intervals[i][1] )]) 
-                #print(sorted_intervals)
-                sorted_intervals.pop(i+1)
-                sorted_intervals.pop(i+1)                
-                #print(sorted_intervals)
+        si = sorted(intervals, key=lambda x: x[0])
+        while i <= len(si) - 2:
+            curr, next, start, end = i, i+1, 0, 1
+            if si[curr][end] >= si[next][start] :
+                #expanding/merging i, i+1th interval if they overlap
+                si.insert(i, [min(si[curr][start],si[next][start]) , max(si[next][end], si[curr][end] )]) 
+                si.pop(i+1) #popping the two intervals which are now merged
+                si.pop(i+1)                
             else:
                 i += 1
         
-        return sorted_intervals
+        return si
