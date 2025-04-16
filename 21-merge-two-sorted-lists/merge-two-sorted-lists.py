1318ce76ab6b61ed12f2f523654a35ce5f5d70fc
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-04-15 19:18:22 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (62.45%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -6,18 +6,42 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
-        dummy = ListNode()
-        curr = dummy
+        dummy = ListNode()       # Dummy node to start the merged list
+        curr = dummy             # Pointer to build the result list
 
-        while list1 is not None or list2 is not None:
-
-            if list1 is not None and (list2 is None or list1.val < list2.val):
+        while list1 and list2:
+            if list1.val < list2.val:
                 curr.next = list1
                 list1 = list1.next
             else:
                 curr.next = list2
                 list2 = list2.next
-
             curr = curr.next
 
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
+
         return dummy.next
+
+        '''
+        Every time we do:
+
+        curr.next = list1 or list2
+        curr = curr.next
+
+        We’re linking nodes to the .next of curr, which is really just building off of the .next of dummy.
+
+        So if you visualize it:
+
+        dummy -> node1 -> node2 -> node3 ...
+        Even though curr keeps moving forward, dummy always stays at the start.
+
+        Why return dummy.next?
+        Because dummy was just a placeholder node (usually with value 0), we don’t want to include it in the final answer.
+
+        So we return:
+        
+        return dummy.next  # The real start of our merged list
+        '''
