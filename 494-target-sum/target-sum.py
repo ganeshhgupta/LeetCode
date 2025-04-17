2e457d2e347d7d61c1a07d1948ccfb7c2b38aa6a
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-04-17 18:24:34 -0500

    Time: 294 ms (9.27%) | Memory: 38.5 MB (17.72%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -5,14 +5,19 @@ class Solution:
         n = len(nums)
         def dfs(i, val):
 
+            #if array ends, check if sum == target, ret 1
             if i == n:
                 return 1 if val == target else 0
 
+            #if (i, val) combo already in dp, return answer already
             if (i, val) in dp:
                 return dp[(i, val)]
 
+            #all possible valid sign combinations from (i,val):
             dp[(i, val)] = ( dfs(i + 1, val  - nums[i]) + dfs(i + 1, val  + nums[i]) )
 
             return dp[(i, val)]
 
-        return dfs(0, 0)
+        res = dfs(0, 0)
+        print(dp)
+        return res
