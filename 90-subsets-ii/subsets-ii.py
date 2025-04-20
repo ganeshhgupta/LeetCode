2cff259aa266dd624d4c2deefa73bac4fb6e347d
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-04-20 14:31:45 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (57.37%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -7,26 +7,19 @@ class Solution:
 
         def dfs(i):
 
-            #returns when last element in nums has been reached, irrespective of how many elements are in subset array
-            #why? cause i will increment irrespective for whether append or pop is happening to subset array
             if i == len(nums):
                 res.append(subset.copy())
                 return
 
-            #subset = [] -> [1] -> [1, 2] ..
-            #                   -> [] -> [2] -> [2, 3]
-            #                                -> [] -> [3] .. so on
 
             subset.append(nums[i])
             dfs(i+1)
-            subset.pop()
-            
-            #to eliminate duplicates
-            while i + 1 < len(nums) and nums[i] == nums[i+1]:
-                i += 1
 
-            #backtracking by going on element back in the existing subset
-            #subset.pop()
+            subset.pop()
+            #to eliminate duplicates
+            curr = nums[i]
+            while i + 1 < len(nums) and curr == nums[i+1]:
+                i += 1
             dfs(i+1)
 
         dfs(0)
