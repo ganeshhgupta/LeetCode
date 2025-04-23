bb7441609f5fc70ebe5f0bc2dd35a50ff4e785ec
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-04-23 15:48:15 -0500

    Time: 7 ms (71.59%) | Memory: 17.8 MB (83.92%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -1,36 +1,18 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        
-        ans = "1"
+        res = "1"
+        for _ in range(1, n):
+            res = self.RLE(res)
+        return res
 
-        if n == 1:
-            return "1"
-
-        else:
-            for i in range(1, n):
-                ans = self.RLE(ans)
-                print(ans)
-
-            return ans
-
-    def RLE(self, n: str) -> str:
-        li = []
-        c = 1
-
-        if len(n) == 1:
-            return "1" + n
-
-        for i in range(len(n)):
-            if i < len(n) - 1:
-
-                if n[i] != n[i + 1]:
-                    li.append(str(c))
-                    li.append(n[i])
-                    c = 1
-                else:
-                    c += 1
+    def RLE(self, s: str) -> str:
+        result = ""
+        count = 1
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                count += 1
             else:
-                li.append(str(c))
-                li.append(n[i])
-
-        return ''.join(li)
+                result += str(count) + s[i - 1]
+                count = 1
+        result += str(count) + s[-1]
+        return result
