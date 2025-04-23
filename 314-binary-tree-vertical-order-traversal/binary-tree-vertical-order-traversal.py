c6a37c1b9767b29b3d7062a1e6685a0f714d1366
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-04-23 17:04:45 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (99.04%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -1,30 +1,20 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        
-        #bfs to preserve height
 
         if not root:
             return []
 
-        cols = defaultdict(list)
-        q = deque([(root, 0)]) #send a tuple
+        map = defaultdict(list)
+        q = deque([(root, 0)])
 
         while q:
-            qLen = len(q)
-            for i in range(qLen):
-                node, col = q.popleft()
-                if node:
-                    cols[col].append(node.val)  #no level list need, direct append to cols[col]
-                if node.left:
-                    q.append([node.left, col - 1])
-                if node.right:
-                    q.append([node.right, col + 1])
-        print( list(cols) )
-        return [cols[x] for x in sorted(cols)]
+            node, col = q.popleft()
+            if node:
+                map[col].append(node.val)
 
+                q.append((node.left, col - 1))
+                q.append((node.right, col + 1))
+        if map:
+            return [map[i] for i in sorted(map)]
+        else:
+            return []
