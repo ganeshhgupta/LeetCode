4e765bffc634dc661b61be9102d557bcb9e3df1d
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-04-23 02:32:50 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.59%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -10,22 +10,18 @@ class Solution:
 
         while len(li) < l:
 
-        #if len(li) <  l:
             for c in range(cs, ce):
                 li.append(matrix[r][c])
             rs += 1
 
-        #if len(li) <  l:
             for r in range(rs, re):
                 li.append(matrix[r][c])
             ce -= 1
 
-        #if len(li) <  l:
             for c in range(ce - 1, cs - 1, -1):
                 li.append(matrix[r][c])
             re -= 1
 
-        #if len(li) <  l:
             for r in range(re - 1, rs - 1, -1):
                 li.append(matrix[r][c])
             cs += 1
