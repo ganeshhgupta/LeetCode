2b77f7275adbb58a3fe5e8852f15e80d9b32fd48
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-04-23 16:20:47 -0500

    Time: 50 ms (70.19%) | Memory: 37.9 MB (89.66%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -2,10 +2,10 @@ class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         mods = {0: -1}
         total = 0
-        
+        k = 1 if k == 0 else k
         for i, num in enumerate(nums):
             total += num
-            mod = total % k if k != 0 else total
+            mod = total % k
             
             if mod in mods:
                 if i - mods[mod] > 1:
