872df349f0768060920233b2e71112f6bfa8bd0b
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-04-24 08:52:24 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (21.50%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -2,19 +2,14 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         
         res = []
-        subset = []
-
-        def dfs(i):
+        def dfs(i, li):
             
             if i == len(nums):
-                res.append(subset.copy())
+                res.append(li.copy())
                 return
 
-            subset.append(nums[i])
-            dfs(i+1) #include nums[i] and go on to explorig subsets which contain nums[i]
+            dfs(i+1, li + [nums[i]]) 
+            dfs(i+1, li)
 
-            subset.pop()
-            dfs(i+1) #exclude nums[i] and go on to explorig subsets which don't contain nums[i]
-
-        dfs(0)
+        dfs(0, [])
         return res
