059bf513191832a823ce0e5b5aa7b0eb0d573657
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-05-14 23:49:03 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (96.38%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        st = []
+        map = {')':'(' , '}':'{' , ']':'['}
+
+        for i in s:
+            if i in map:
+                if st and st[-1] == map[i]:
+                    st.pop()
+                else:
+                    return False
+            else:
+                st.append(i)
+        
+        return True if not st else False
