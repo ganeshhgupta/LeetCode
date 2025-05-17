50b125793c7f70c3fd065e55ad16ac1c49386f6d
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-05-17 12:05:56 -0500

    Time: 35 ms (86.83%) | Memory: 18.2 MB (83.32%) - LeetSync

--- 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -1,40 +1,32 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-"""
-
 class Solution:
     def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
+
         if not root:
-            return None
+            return
+        
+        head, tail = None, None
 
-        stack = []
-        curr = root
-        head = None   #left most node
-        prev = None   #stores the last visited node
+        def dfs(node):
+            nonlocal head, tail
+            if not node:
+                return
+            
+            dfs(node.left)
 
-        while curr or stack:
-            while curr:
-                stack.append(curr)
-                curr = curr.left  #traverse and store all left nodes from current root
+            if tail:
+                tail.right = node
+                node.left = tail
+            else:
+                head = node
 
-            node = stack.pop()
+            tail = node
+            
+            dfs(node.right)
+        
 
-            if not head:
-                head = node  # First node we visit in-order is the smallest one
-            if prev:
-                prev.right = node
-                node.left = prev
+        dfs(root)
 
-            prev = node  # Mark current node as the last visited
-            curr = node.right  # Go to the right subtree
-
-        # Now connect head and tail to make the list circular
-        head.left = prev
-        prev.right = head
-
+        head.left = tail
+        tail.right = head
+        
         return head
