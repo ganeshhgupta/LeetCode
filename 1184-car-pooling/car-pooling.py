7902fc057b0f3f570f72376122d31ceb7ed609c6
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-05-17 14:08:44 -0500

    Time: 5 ms (22.01%) | Memory: 18.3 MB (51.16%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,20 +1,15 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        
+
         li = []
         for t in trips:
             p, s, e = t
             li.append([s, p])
-            li.append([e, p * -1])
-
+            li.append([e, -p])
         li = sorted(li)
-        print(li)
-        
         curr = 0
-        for i in li:
-            _, p = i
-            curr += p
-            print(curr)
+        for i, j in li:
+            curr += j
             if curr > capacity:
                 return False
         return True
