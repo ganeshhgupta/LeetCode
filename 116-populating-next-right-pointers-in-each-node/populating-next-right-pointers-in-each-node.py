66e79527a6a3934625ccb7a63715c48bfb07efea
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-05-17 14:03:07 -0500

    Time: 59 ms (13.83%) | Memory: 19.3 MB (35.07%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -1,37 +1,23 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
-"""
-
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        
-        if not root:
-            return None
 
-        q = collections.deque()
-        q.append(root)
-        
+        q = deque([root])
+        res = []
+
         while q:
-            l = len(q)
-            level = []
-            for i in range(l):
-                node = q.popleft()
-                if node:
-                    level.append(node)
-                    if node.left:
-                        q.append(node.left)
-                    if node.right:
-                        q.append(node.right)
-
-            if level:
-                for i in range(len(level) - 1):
-                    level[i].next = level[i+1]
-                level[-1].next = None
-
+            li = []
+            for i in range(len(q)):
+                n = q.popleft()
+                if n:
+                    li.append(n)
+                    q.append(n.left)
+                    q.append(n.right)
+            
+            if li:
+                
+                for i in range(len(li) - 1):
+                    li[i].next = li[i+1]
+                li[-1].next = None
+                res.append(li)
+        
         return root
