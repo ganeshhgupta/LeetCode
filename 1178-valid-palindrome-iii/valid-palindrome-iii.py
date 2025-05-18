37e63025b4f0782af42c5fed1d9686fd6a13651c
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-05-18 18:31:46 -0500

    Time: 1576 ms (10.26%) | Memory: 257.9 MB (28.88%) - LeetSync

--- 1178-valid-palindrome-iii/valid-palindrome-iii.py
+++ 1178-valid-palindrome-iii/valid-palindrome-iii.py
@@ -1,23 +1,22 @@
 class Solution:
     def isValidPalindrome(self, s: str, k: int) -> bool:
-        memo = {}
+        
+        m = {}
 
         def pal(i, j, k):
-            if (i, j, k) in memo:
-                return memo[(i, j, k)]
-
+            if (i, j, k) in m:
+                return m[(i, j, k)]
+            
             while i < j:
                 if s[i] != s[j]:
                     if k == 0:
-                        memo[(i, j, k)] = False
+                        m[(i, j, k)] = False
                         return False
-                    res = pal(i + 1, j, k - 1) or pal(i, j - 1, k - 1)
-                    memo[(i, j, k)] = res
-                    return res
-                i += 1
-                j -= 1
-
-            memo[(i, j, k)] = True
+                    m[(i, j, k)] = pal(i+1, j, k-1) or pal(i, j-1, k-1)
+                    return m[(i, j, k)]
+                i, j = i+1, j-1
+            
+            m[(i, j, k)] = True
             return True
-
-        return pal(0, len(s) - 1, k)
+        
+        return pal(0, len(s)-1, k)
