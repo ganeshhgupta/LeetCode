cd1171c6d9c4a102d0a8be8d0c2bfb81aa775cb6
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-05-19 17:11:34 -0500

    Time: 139 ms (14.67%) | Memory: 21.7 MB (9.74%) - LeetSync

--- 971-shortest-bridge/shortest-bridge.py
+++ 971-shortest-bridge/shortest-bridge.py
@@ -1,42 +1,48 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         
+        #revise
+        #1. bruteforce untill finds a single '1'
+        #2. dfs out the entire island and add to visited
+        #3. use visit hashset as a queue
+        #4. do bfs from queue untill second island is reached
+
         N = len(grid)
         dirs = [[0,1], [0,-1], [-1,0], [1,0]]
         visit = set()
 
-        def invalid(r,c):
-            if min(r,c) < 0 or max(r,c) == N:
+        def invalid(r, c):
+            if min(r, c) < 0 or max(r, c) == N:
                 return True
-            else:
-                return False
-
+            return False
+        
         def dfs(r, c):
-            if invalid(r, c) or not grid[r][c] or (r, c) in visit:
+            if invalid(r, c) or grid[r][c] == 0 or (r, c) in visit:
                 return
-            visit.add((r,c))
+            visit.add((r, c))
             for dr, dc in dirs:
                 dfs(r + dr, c + dc)
-
+        
         def bfs():
-            res = 0
             q = deque(visit)
+            res = 0
             while q:
                 for i in range(len(q)):
                     r, c = q.popleft()
                     for dr, dc in dirs:
-                        curr_r, curr_c = r + dr, c + dc
-                        if invalid(curr_r, curr_c) or (curr_r, curr_c) in visit:
+                        nr, nc = r + dr, c + dc
+                        if invalid(nr, nc) or (nr, nc) in visit:
                             continue
-                        if grid[curr_r][ curr_c]:
+                        if grid[nr][nc] == 1: #2nd island found
                             return res
-                        q.append([curr_r, curr_c])
-                        visit.add((curr_r, curr_c))
+                        q.append([nr, nc])
+                        visit.add((nr, nc))
                 res += 1
-
+                
         for r in range(N):
             for c in range(N):
-                if grid[r][c]:
+                if grid[r][c] == 1:
                     dfs(r, c)
                     return bfs()
+
 
