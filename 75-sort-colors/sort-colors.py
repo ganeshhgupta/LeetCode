7aab7ba9c9932b79cc8cf21e7cdbf94335006251
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-05-19 11:25:56 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.57%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,19 +1,17 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         
-        left, i, right = 0, 0, len(nums) - 1
-
-        #bubble all 0s to left, 2s to right
-        while i <= right:
+        l, i, r = 0, 0, len(nums) - 1
+        
+        while i < r + 1:
             if nums[i] == 0:
-                nums[i], nums[left] = nums[left], nums[i]
-                left += 1
-                i += 1
-            elif nums[i] == 2:
-                nums[i], nums[right] = nums[right], nums[i]
-                right -= 1
-            else:  # nums[i] == 1
-                i += 1
-
-    #Why no i += 1 after swapping with right?
-    #Because you don't know what nums[i] is after the swap — it could be a 0 or 2 that still needs to be sorted.
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+            i += 1
+        i = r
+        while i >= l:
+            if nums[i] == 2:
+                nums[r], nums[i] = nums[i], nums[r]
+                r -= 1
+            i -= 1
+        return nums
