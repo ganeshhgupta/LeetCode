130b238d50a30621721ad257b357a6ab243f4783
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-05-19 11:07:26 -0500

    Time: 7 ms (87.00%) | Memory: 19.7 MB (61.03%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -1,5 +1,3 @@
-from typing import List
-
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         if not mat or not mat[0]:
@@ -8,31 +6,22 @@ class Solution:
         rows, cols = len(mat), len(mat[0])
         res = []
         r = c = 0
-        direction = 1  #1 means moving up-right, -1 means down-left
+        dir = 1  #1 means moving up-right, -1 means down-left
 
         for _ in range(rows * cols):
             res.append(mat[r][c])
-            if direction == 1:
-                #priority : right bound, top bound
+            if dir == 1:
                 if c == cols - 1:
-                    r += 1
-                    direction = -1
+                    r, dir = r+1, -1
                 elif r == 0:
-                    c += 1
-                    direction = -1
+                    c, dir = c+1, -1
                 else:
-                    r -= 1
-                    c += 1
+                    r, c = r-1, c+1
             else:
-                #priority : bottom bound, left bound
                 if r == rows - 1:
-                    c += 1
-                    direction = 1
+                    c, dir = c+1, 1
                 elif c == 0:
-                    r += 1
-                    direction = 1
+                    r, dir = r+1, 1
                 else:
-                    r += 1
-                    c -= 1
-
+                    r, c = r+1, c-1
         return res
