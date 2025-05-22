605eea6fde4f0df10d31958131a1aa2f41617c29
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-05-22 02:03:26 -0500

    Time: 11 ms (51.18%) | Memory: 19.1 MB (47.94%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,38 +1,11 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l = 0
+        r = len(arr) - 1
 
-        def binary_search(arr, x):
-            l, r = 0, len(arr) - 1  
-
-            while l <= r:
-                mid = l + (r - l) // 2
-                if arr[mid] == x:
-                    return mid 
-                elif arr[mid] < x:
-                    l = mid + 1  
-                else:
-                    r = mid - 1  
-            return l
-
-        i = binary_search(arr, x)  #closest insert position of x
-
-        l, r = i - 1, i
-        res = []
-
-        while len(res) < k:
-            ld = rd = float('inf')
-
-            if l >= 0:
-                ld = abs(arr[l] - x)
-
-            if r < len(arr):
-                rd = abs(arr[r] - x)
-
-            if ld <= rd:
-                res.append(arr[l])
-                l -= 1 
+        while r - l >= k:
+            if abs(arr[l] - x) > abs(arr[r] - x):
+                l += 1
             else:
-                res.append(arr[r])
-                r += 1 
-
-        return sorted(res)
+                r -= 1
+        return arr[l : r + 1]
