47aeb55278283183878bdbaf44fb3d06f3b7c5be
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-08-09 21:56:48 -0700

    Time: 3 ms (59.16%) | Memory: 17.8 MB (53.05%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        n_count = Counter(str(n))
+        for i in range(31):
+            if Counter(str(1 << i)) == n_count:
+                return True
+        return False
