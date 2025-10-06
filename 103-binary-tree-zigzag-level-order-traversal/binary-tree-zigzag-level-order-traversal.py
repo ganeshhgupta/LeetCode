136529a10e3f32f1096906651e53a5fa3f6e8e89
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-05 22:46:00 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (76.99%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -1,46 +1,36 @@
+from collections import deque
+from typing import List, Optional
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        
-        res = []
 
-        q = deque() #double ended queue
-        q.append(root) #append first level
-        order = 'lr'
+        # O(n), O(n)
+        if not root:
+            return []
+
+        q = deque([root])
+        res = []
+        dir = 1
 
         while q:
-            q_len = len(q) #no. of elements in q at any given level
-            level = [] #to store all children from next level
+            level = []
+            for _ in range(len(q)):
+                n = q.popleft()
+                if n:
+                    level.append(n.val)
+                    if n.left: q.append(n.left)
+                    if n.right: q.append(n.right)
 
-            for i in range(q_len):
-
-                node = q.popleft() #note popleft
-
-                #for every popped node at a level, append its children in q
-                #q_len with take care we never traverse more than len of prev level
-                if node:
-                    if order == 'lr':
-                        print("left to right")
-                        level.append(node.val)
-                        q.append(node.left)
-                        q.append(node.right)
-                    else:
-                        print("right to left")
-                        level.insert(0, node.val)
-                        q.append(node.left)
-                        q.append(node.right)
-            # after every iteration, append level list to main list
-            if level:
-                print(level)
-                res.append(level)
-                if order == 'lr':
-                    order = 'rl'
-                else:
-                    order = 'lr'
+            if dir == -1:
+                level.reverse()
+            res.append(level)
+            dir *= -1
 
         return res
