aba0d1804796fe24ea6d09023fa9ea7483d4d62d
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-07 16:41:52 -0500

    Time: 785 ms (92.56%) | Memory: 22.1 MB (81.72%) - LeetSync

--- 1122-longest-duplicate-substring/longest-duplicate-substring.py
+++ 1122-longest-duplicate-substring/longest-duplicate-substring.py
@@ -1,16 +1,37 @@
 class Solution:
     def longestDupSubstring(self, s: str) -> str:
-        left = 0
-        right = 1
-        res = ""
+        
+        # O(n log n), O(n)
         n = len(s)
-        while right<n:
-            if s[left:right] in s[left+1:]:
-                if right - left > len(res):
-                    res = s[left:right]
-                right+=1
-                continue
-            left+=1
-            if left == right:
-                right+=1
-        return res
+        mod = 2**63 - 1
+        nums = [ord(c) - ord('a') for c in s]
+        print(nums)
+
+        def dup(L):
+            h = 0
+            for i in range(L):
+                h = (h * 26 + nums[i]) % mod
+            seen = {h}
+            baseL = pow(26, L, mod)
+            for i in range(L, n):
+                h = (h * 26 - nums[i - L] * baseL + nums[i]) % mod
+                if h in seen:
+                    return i - L + 1  # return starting index
+                seen.add(h)
+            return -1
+
+        l, r = 1, n
+        res_start = 0
+        res_len = 0
+
+        while l <= r:
+            mid = l + (r - l) // 2
+            candidate = dup(mid)
+            if candidate != -1:
+                res_start = candidate
+                res_len = mid
+                l = mid + 1  # try longer
+            else:
+                r = mid - 1  # try shorter
+
+        return s[res_start:res_start + res_len]
