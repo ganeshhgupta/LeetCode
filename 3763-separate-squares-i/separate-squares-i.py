cb55547e2230a3561909db77dbe1de8f04538445
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-06 19:21:18 -0500

    Time: 424 ms (93.84%) | Memory: 46.3 MB (87.11%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.py
+++ 3763-separate-squares-i/separate-squares-i.py
@@ -2,21 +2,21 @@ class Solution:
     def separateSquares(self, squares: List[List[int]]) -> float:
 
         # O(n log n), O(n) n:Sqaures
-        A = sum((l * l for x, y, l in squares))
+        A = sum((l * l for x, y, l in squares)) / 2
         strips = [(y, 1, l) for x, y, l in squares] + [(y + l, 0, l) for x, y, l in squares]
         # (y, 1, l) here 1 -> start of sqaure, (y + l, 0, l) 0 -> end
         strips.sort()
-        width = curr_a = prev_h = 0
+        w = curr_a = prev_h = 0 # w = width of strip
 
         for i in range(len(strips)):
             y, start, l = strips[i]
             h_diff = y - prev_h
-            a_diff = width * h_diff
+            a_diff = w * h_diff
 
-            if curr_a + a_diff >= A / 2:
-                req_h = (A / 2 - curr_a) / width
+            if curr_a + a_diff >= A:
+                req_h = (A - curr_a) / w
                 return prev_h + req_h
             
-            width += l if start == 1 else -l
+            w += l if start == 1 else -l
             curr_a += a_diff
             prev_h = y
