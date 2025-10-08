afe22b0e224c9103eba1bd0b1bb57812413b7cd9
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-08 17:59:29 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.39%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.py
+++ 29-divide-two-integers/divide-two-integers.py
@@ -1,32 +1,25 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
         
-        if divisor == 0:
-            return -1
+        # O((log(a/b))2), O(1)
+        # youtube.com/watch?v=xefkgtd44hg
 
-        if dividend == divisor:
-            return 1
-        if dividend == -2**31 and divisor == -1:
-            return (2**31) - 1 
+        if dividend == 0: return 0
         
-        if divisor == 1:
-            return dividend
+        a, b = abs(dividend), abs(divisor)
+        sign = (dividend//a) * (divisor//b)
+        res = 0
 
-        q = 0
-        sign = ( abs(divisor)//divisor )
-        if dividend != 0:
-            sign *= ( abs(dividend)//dividend ) 
+        while a >= b:
+            c, d = 1, b # counter and decrement
 
-        divisor, dividend = abs(divisor), abs(dividend)
-        while dividend >= divisor:
-            temp = divisor
-            mul = 1
-            while dividend >= temp:
-                dividend -= temp
-                q += mul
-                temp += temp
-                mul *= 2
-        print(q * sign)
-        print(2**31)
-        #return q*sign
-        return min (2**31, max(- 2**31,  q * sign))
+            while a >= d:
+                a -= d
+
+                res += c
+                c += c
+                d += d
+        
+        res = res * sign
+        return min( max( -2**31, res), 2**31 - 1)
+
