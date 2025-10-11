57d9c07b0289fec9b6e557e7ff6892675b167c80
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-11 13:23:04 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.67%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,17 +1,26 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        
-        l, i, r = 0, 0, len(nums) - 1
-        
-        while i < r + 1:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        # O(n), O(1) Dutch National Flag algorithm
+        i, l, r = 0, 0, len(nums) - 1
+
+        while i <= r:
+
             if nums[i] == 0:
                 nums[i], nums[l] = nums[l], nums[i]
+                i += 1
                 l += 1
-            i += 1
-        i = r
-        while i >= l:
-            if nums[i] == 2:
-                nums[r], nums[i] = nums[i], nums[r]
+
+            elif nums[i] == 1:
+                i += 1
+
+            else:
+                nums[i], nums[r] = nums[r], nums[i]
                 r -= 1
-            i -= 1
-        return nums
+
+
+        
+        
