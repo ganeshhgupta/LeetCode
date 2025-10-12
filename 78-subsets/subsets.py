c99f74230533c262c6ae344834754863160f9a70
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-12 18:31:11 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (39.71%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,13 +1,25 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         
+        # O(2ⁿ), O(n)
         res = []
+        def dfs(i, li):                 # without li.pop()
+            if i == len(nums):
+                res.append(li)
+                return
+            
+            dfs(i + 1, li + [nums[i]])  # directly change while passing through fn
+            dfs(i + 1, li)
+
         def dfs(i, li):
             if i == len(nums):
                 res.append(li.copy())
-                return 
+                return
             
-            dfs(i+1, li+[nums[i]])
-            dfs(i+1, li)
+            li.append(nums[i])              # include nums[i]
+            dfs(i + 1, li)
+            li.pop()                        # backtrack
+            dfs(i + 1, li)                  # exclude nums[i]
+
         dfs(0, [])
         return res
