0739d18e41a5619ecc123e0d2b501b79a56a720d
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-18 20:36:20 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (69.28%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,26 +1,26 @@
-from typing import List
-
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-
+        
+        # O(4ⁿ), O(4ⁿ)
+        
         if not digits:
             return []
-
+        
         map = {
-            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
-            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+            '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+            '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
         }
 
+        n = len(digits)
         res = []
 
         def dfs(i, li):
-            if i == len(digits):
+            if i == n:
                 res.append(li)
                 return
 
-            letters = map[digits[i]]
-            for l in letters:
-                dfs(i + 1, li + l)
+            for j in map[digits[i]]:
+                dfs(i + 1, li + j)
 
         dfs(0, "")
         return res
