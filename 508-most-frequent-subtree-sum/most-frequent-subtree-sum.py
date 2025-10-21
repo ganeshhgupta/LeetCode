91b03762169d206c98d3c345b3261fe5fa9b1a0a
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-21 14:22:40 -0500

    Time: 3 ms (84.49%) | Memory: 20.3 MB (83.00%) - LeetSync

---
+++ 508-most-frequent-subtree-sum/most-frequent-subtree-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findFrequentTreeSum(self, root: Optional[TreeNode]) -> List[int]:
+        
+        sum_count = defaultdict(int)
+
+        def dfs(n):
+            if not n: return 0
+
+            
+            curr = dfs(n.left) + n.val + dfs(n.right)
+
+            sum_count[curr] += 1
+
+            return curr
+        
+        dfs(root)
+        
+        res = max(sum_count.values())
+        return [s for s, freq in sum_count.items() if freq == res]
