37a182e3e6eb373ab9aaddcca6c3e3ae8a5735c9
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-23 10:49:55 -0500

    Time: 387 ms (43.42%) | Memory: 18.4 MB (53.39%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,20 +1,19 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         
-        memo = {}
+        # O(n . amount), O(amount) 
+        dp = [0] * ( amount + 1 )
+        dp[0] = 1
 
-        def dfs(i, cur):
+        for i in range(len(coins) - 1, -1, -1):
+            new_dp = [0] * ( amount + 1 )
+            new_dp[0] = 1
 
-            if i >= len(coins) or cur > amount: 
-                return 0
+            for j in range(1, amount + 1):
+                new_dp[j] = dp[j]
 
-            if cur == amount: 
-                return 1
-
-            if (i, cur) in memo: 
-                return memo[(i, cur)]
+                if j - coins[i] >= 0:
+                    new_dp[j] += new_dp[j - coins[i]]
             
-            memo[(i, cur)] = dfs(i, cur + coins[i]) + dfs(i + 1, cur)
-            return memo[(i, cur)]
-
-        return dfs(0, 0)
+            dp = new_dp
+        return dp[amount]
