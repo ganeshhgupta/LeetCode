5af0a50986a4fd1b7f90a2bc199e02543b313568
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-28 00:15:55 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (54.21%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,24 +4,19 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        # O(n), O(1), easy peasy 
-        
-        zeros = ones = 0
+        # O(n), O(1), dutch flag
 
-        for i in nums:
-            if i == 0:
-                zeros += 1
-            elif i == 1:
-                ones += 1
-        
-        i = 0
-        while i < len(nums):
-            if zeros > 0:
-                nums[i] = 0
-                zeros -= 1
-            elif ones > 0: 
-                nums[i] = 1
-                ones -= 1
+        l, i, r = 0, 0, len(nums) - 1
+
+        while i <= r:
+            
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+                i += 1
+            elif nums[i] == 1:
+                i += 1
             else:
-                nums[i] = 2
-            i += 1
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+            
