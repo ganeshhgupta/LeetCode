34247356a76c4083e6b4c16f9913abc14d772e1a
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-28 10:27:28 -0500

    Time: 12 ms (25.10%) | Memory: 17.8 MB (63.40%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -1,27 +1,50 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-
-        # O(m.n), O(m.n)
-        dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
-        dp[0][0] = True
         
-        for j in range(1, len(p) + 1):
-            if p[j-1] == "*":
-                dp[0][j] = dp[0][j-2]
+        # '*' matches 0 or any number of PRECEEDING element
+        m, n = len(s) + 1, len(p) + 1
+        dp = [[False] * n for _ in range(m)] # p across cols, s across rows
+        dp[0][0] = True
 
-        for i in range(1, len(s) + 1):
-            for j in range(1, len(p) + 1):
+        # intialize first row
+        # when we encounter '*', '*' and the char preceeding it can be skipped like they didn't even exist, 
+        # since we skip two chars, so simply pick the last value two cells from left, dp[0][j-2]
+        for j in range(2, n): 
+                    if p[j-1] == '*':
+                        dp[0][j] = dp[0][j-2]
+
+        for i in range(1, m):
+            for j in range(1, n):
+
+                if p[j-1] == '.' or s[i-1] == p[j-1]:
+                    dp[i][j] = dp[i-1][j-1] # top left diag
+
+                elif p[j-1] == '*':
+                    dp[i][j] = dp[i][j-2] # pick from 2 cells to left
+                                          # cause dp[i][j-1]th char could be skipped, so pick last legit True?False value from dp[i][j-2]
+
+                    if p[j-2] == '.' or p[j-2] == s[i-1]: # '.*' match any char any no. of times
+                        dp[i][j] = dp[i][j] or dp[i-1][j] # keep as is or pick from top
+        
+        return dp[m-1][n-1]
+
                 
-                if p[j-1] == "." or p[j-1] == s[i-1]:
-                    dp[i][j] = dp[i-1][j-1]
-                    
-                elif p[j-1] == "*":
-                    dp[i][j] = dp[i][j-2] # zero occurence of p[i-1]
-
-                    if p[j-2] == "." or p[j-2] == s[i-1]: 
-                        # if '.*' come together, they accomodate any/all characters
-                        # character preceding '*' matches the current text character s[i-1]
-
-                        dp[i][j] = dp[i][j] or dp[i-1][j]
-                        
-        return dp[len(s)][len(p)]
+'''
+p  :  ∅  f  a  *  n  .  m
+      0  1  2  3  4  5  6
+    ┌──┬──┬──┬──┬──┬──┬──┬──┐
+ ∅ 0│ T│ F│ F│ F│ F│ F│ F│
+    ├──┼──┼──┼──┼──┼──┼──┼──┤
+ f 1│ F│ T│ F│ T│ F│ F│ F│
+    ├──┼──┼──┼──┼──┼──┼──┼──┤
+ a 2│ F│ F│ T│ T│ F│ F│ F│
+    ├──┼──┼──┼──┼──┼──┼──┼──┤
+ a 3│ F│ F│ F│[T]│F│ F│ F│
+    ├──┼──┼──┼──┼──┼──┼──┼──┤
+ m 4│ F│ F│ F│ F│ T│ F│ F│
+    ├──┼──┼──┼──┼──┼──┼──┼──┤
+ a 5│ F│ F│ F│ F│ F│ T│ F│
+    ├──┼──┼──┼──┼──┼──┼──┼──┤
+ m 6│ F│ F│ F│ F│ F│ F│ T│
+    └──┴──┴──┴──┴──┴──┴──┴──┘
+'''
