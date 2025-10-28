6450235f38afc7f2986aecd78a3fb4bb1ef82ae5
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-28 14:07:39 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (32.14%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/unique-binary-search-trees.py
@@ -1,13 +1,21 @@
 class Solution:
     def numTrees(self, n: int) -> int:
 
+        # Catalan sequence: 1, 1, 2, 5, 14, 42, 132, 429, ...
         # O(n²), O(n)
 
-        dp = [0] * (n + 1)
-        dp[0] = 1
-        
-        for i in range(1, n + 1):
-            for j in range(i):
-                dp[i] += dp[j] * dp[i - 1 - j]
-        
-        return dp[n]
+        res = 1
+        for i in range(n):
+            res = res * 2 * (2 * i + 1) // (i + 2)
+        return res
+
+        '''
+        The Catalan numbers show up in MANY problems:
+        - Number of unique BSTs with n nodes (this problem!)
+        - Number of valid parentheses combinations
+        - Number of ways to triangulate a polygon
+        - Number of paths in a grid (without crossing)
+        - Number of ways to arrange items in a stack
+
+        Formula: C(n) = C(0)*C(n-1) + C(1)*C(n-2) + ... + C(n-1)*C(0)
+        '''
