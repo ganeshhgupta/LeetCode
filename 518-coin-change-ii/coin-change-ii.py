5ffbcebe0eac090b87ffb022572e5cecd454d6f6
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-28 11:57:17 -0500

    Time: 199 ms (70.86%) | Memory: 17.9 MB (79.63%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,19 +1,12 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        
-        # O(n . amount), O(amount) 
-        dp = [0] * ( amount + 1 )
+
+        # O(amount × coins), O(amount)
+        dp = [0] * (amount + 1)
         dp[0] = 1
-
-        for i in range(len(coins) - 1, -1, -1):
-            new_dp = [0] * ( amount + 1 )
-            new_dp[0] = 1
-
-            for j in range(1, amount + 1):
-                new_dp[j] = dp[j]
-
-                if j - coins[i] >= 0:
-                    new_dp[j] += new_dp[j - coins[i]]
-            
-            dp = new_dp
+        
+        for c in coins:
+            for i in range(c, amount + 1):
+                dp[i] += dp[i - c] # only difference being '+=' instead of '='
+        
         return dp[amount]
