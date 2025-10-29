f0ea4797bbe105d11cc320077db3d7cd462d0785
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-29 09:16:26 -0500

    Time: 14 ms (21.82%) | Memory: 18 MB (27.43%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -1,50 +1,46 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         
-        # '*' matches 0 or any number of PRECEEDING element
+        # O(mn), O(mn)
+        # youtube.com/watch?v=mNbzDlGKmLs
+
         m, n = len(s) + 1, len(p) + 1
         dp = [[False] * n for _ in range(m)] # p across cols, s across rows
         dp[0][0] = True
 
-        # intialize first row
-        # when we encounter '*', '*' and the char preceeding it can be skipped like they didn't even exist, 
-        # since we skip two chars, so simply pick the last value two cells from left, dp[0][j-2]
         for j in range(2, n): 
-                    if p[j-1] == '*':
-                        dp[0][j] = dp[0][j-2]
+            if p[j-1] == '*':
+                dp[0][j] = dp[0][j-2]
 
+        print(dp)
         for i in range(1, m):
             for j in range(1, n):
 
-                if p[j-1] == '.' or s[i-1] == p[j-1]:
-                    dp[i][j] = dp[i-1][j-1] # top left diag
+                if p[j-1] in [s[i-1], '.' ]:
+                    dp[i][j] = dp[i-1][j-1] # pick from top left diag
 
                 elif p[j-1] == '*':
-                    dp[i][j] = dp[i][j-2] # pick from 2 cells to left
-                                          # cause dp[i][j-1]th char could be skipped, so pick last legit True?False value from dp[i][j-2]
+                    dp[i][j] = dp[i][j-2] # pick from two cells left
 
-                    if p[j-2] == '.' or p[j-2] == s[i-1]: # '.*' match any char any no. of times
-                        dp[i][j] = dp[i][j] or dp[i-1][j] # keep as is or pick from top
-        
+                    if p[j-2] in [ s[i-1], '.' ] : # if in a given row, the p[char] to the left of '*' matchs curr s[char]
+                        dp[i][j] = dp[i][j] or dp[i-1][j] # pick from top
+        print(dp)
         return dp[m-1][n-1]
 
-                
 '''
-p  :  ∅  f  a  *  n  .  m
-      0  1  2  3  4  5  6
-    ┌──┬──┬──┬──┬──┬──┬──┬──┐
- ∅ 0│ T│ F│ F│ F│ F│ F│ F│
-    ├──┼──┼──┼──┼──┼──┼──┼──┤
- f 1│ F│ T│ F│ T│ F│ F│ F│
-    ├──┼──┼──┼──┼──┼──┼──┼──┤
- a 2│ F│ F│ T│ T│ F│ F│ F│
-    ├──┼──┼──┼──┼──┼──┼──┼──┤
- a 3│ F│ F│ F│[T]│F│ F│ F│
-    ├──┼──┼──┼──┼──┼──┼──┼──┤
- m 4│ F│ F│ F│ F│ T│ F│ F│
-    ├──┼──┼──┼──┼──┼──┼──┼──┤
- a 5│ F│ F│ F│ F│ F│ T│ F│
-    ├──┼──┼──┼──┼──┼──┼──┼──┤
- m 6│ F│ F│ F│ F│ F│ F│ T│
-    └──┴──┴──┴──┴──┴──┴──┴──┘
+strings are 0-indexed:
+
+s = a b b b c 	p = a b * c
+    0 1 2 3 4	    0 1 2 3
+
+dp is 1-indexed:
+ 	        0      1      2      3
+        0 x    1 a    2 b    3 *    4 d
+  0 x [[True, False, False, False, False], 
+0 1 a [ False, True, False, True, False], 
+1 2 b [ False, False, True, True, False], 
+2 3 b [ False, False, False, True, False], 
+3 4 b [ False, False, False, True, False], 
+4 5 c [ False, False, False, False, True]]
+
 '''
