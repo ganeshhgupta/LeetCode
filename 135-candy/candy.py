fb9d5c49aa75287ac0aca97b8516518132ee6dde
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-10-29 23:50:42 -0500

    Time: 15 ms (72.08%) | Memory: 19.8 MB (69.78%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,16 +1,19 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-
+        
         # O(n), O(n)
+        
         n = len(ratings)
-        candies = [1] * n
+        dp = [1] * n
+
+        for i in range(1, n):
+            if ratings[i-1] < ratings[i]:
+                dp[i] = dp[i-1] + 1
+
+        for i in range(n-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                dp[i] = max( dp[i], dp[i+1] + 1 )
+                # in cases where both left and right slopes apply (like a mountain), 
+                # we need to keep the maximum of both directions, not just overwrite.
         
-        # Left to Right 1st Pass
-        for i in range(1,n): # kids with a higher than their left neighbor get +1 candy
-            if ratings[i] > ratings[i-1]: candies[i] = candies[i-1]+1
-        
-        # Right to Left 2nd Pass
-        for i in range(n-2,-1,-1): # kids with a higher than their right neighbor get +1 candy
-            if ratings[i] > ratings[i+1]: candies[i] = max(candies[i], candies[i+1]+1)
-        
-        return sum(candies)
+        return sum(dp)
