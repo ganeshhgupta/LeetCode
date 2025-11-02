088290dae4659b114a483d30298ac1ffa9bc68ce
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-11-02 11:45:25 -0600

    Time: 0 ms (100.00%) | Memory: 18.1 MB (7.92%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,28 +1,31 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        count = Counter(s)        
-        count = sorted(count.items(), key=lambda x: -x[1])
         
-        if count[0][1] > (len(s) + 1) // 2:
-            return ""
+        # O(nlogk), O(n)
         
+        c = Counter(s)
         res = []
-        while count:
-            char, freq = count.pop(0)
+
+        for k, v in c.items():
+            if v > (len(s) + 1) // 2:
+                return ""
+
+        
+        # max heap (hence -ve sign)
+        mh = [(-v, k) for k, v in c.items()] 
+        heapq.heapify(mh)
+        prev = (0, '')
+
+        while mh:
+
+            count, char = heapq.heappop(mh)
             res.append(char)
+
+            count += 1 # since -ve, we +1 instead of -1
+
+            if prev[0] < 0:
+                heapq.heappush(mh, prev)
             
-            if freq > 1:
-                count.append((char, freq - 1))
-            
-            if not count:
-                break
-            
-            char2, freq2 = count.pop(0)
-            res.append(char2)
-            
-            if freq2 > 1:
-                count.append((char2, freq2 - 1))
-            
-            count = sorted(count, key=lambda x: -x[1])
+            prev = (count, char)
 
         return "".join(res)
