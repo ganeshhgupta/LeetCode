67dcee13bee4e584e141d929a3f676237a52d7ee
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-11-03 10:38:11 -0600

    Time: 16 ms (18.79%) | Memory: 18.2 MB (53.92%) - LeetSync

--- 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.py
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.py
@@ -1,14 +1,19 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-        boxTypes.sort(key=lambda x: x[1], reverse=True)
+        
+        # O(n), O(n)
 
+        heap = [(-units, size) for size, units in boxTypes] # max-heap
+        heapq.heapify(heap)
+        
         res = 0
-        for n, u in boxTypes:
-            if truckSize == 0:
-                break
-
-            boxes_to_take = min(truckSize, n)
-            res += boxes_to_take * u
-            truckSize -= boxes_to_take
-
+        
+        while truckSize > 0 and heap:
+            neg_units, size = heapq.heappop(heap)
+            units = -neg_units
+            
+            take = min(size, truckSize)
+            res += take * units
+            truckSize -= take
+        
         return res
