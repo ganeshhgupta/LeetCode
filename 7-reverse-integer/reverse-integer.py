f7d144cc80a2c3523ebaf12f306d04bd635ce339
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-11-08 12:40:15 -0600

    Time: 36 ms (74.21%) | Memory: 17.9 MB (16.77%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,24 +1,19 @@
 class Solution:
     def reverse(self, x: int) -> int:
         
-        # O(n), O(n)
-        MAX, MIN =  (2 ** 31) -1  , (2 ** 31) * -1 
-        sign = -1 if x < 0 else 1
-
-        x = list (str( abs(x) ))
-        x = x[::-1]
+        if x == 0:
+            return 0
         
-        i = 0
-        while i < len(x) and x[i] == '0':
-            i += 1
+        sign = 1
+        if x < 0:
+            sign = -1
+            x = -x
         
-        x = x[i:]
-        if x == []: return 0
-        x = int("".join(x)) * sign
-
-        return x if MIN <= x <= MAX else 0
-
-
-
-
-    
+        res = int(str(x)[::-1])
+        
+        res *= sign
+        
+        if res < -2**31 or res > 2**31 - 1:
+            return 0
+        
+        return res
