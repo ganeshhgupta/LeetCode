1ffd8146c9dccdcddc932228d33e2254d79aa40d
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-11-09 19:35:50 -0600

    Time: 22 ms (88.79%) | Memory: 35.6 MB (5.05%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,30 +1,19 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        
-        if not nums or 1 not in nums:
+        # Keep only positive numbers
+        res = [n for n in nums if n > 0]
+        if not res:
             return 1
-
-        #convert all negatives and zeroes and numbers greater than len(nums) to 1
-
-        for i in range(len(nums)):
-            if nums[i] <= 0 or nums[i] > len(nums):
-                nums[i] = 1
-
-        #now that we don't have any negatives in the array,
-        #for every num at ith index, flip the num-th index to negative
-
-        for i, num in enumerate(nums):
-            to_change = nums[abs(num) - 1]
-
-            if to_change > 0:
-                nums[abs(num) - 1] *= -1
-
-        #after above operation is done, the first positive element encountered (+1) would be the answer
-
-        for i in range(len(nums)):
-            if nums[i] > 0:
-                return i + 1
-
-        #else, answer would be len(nums) + 1
-
-        return len(nums) + 1
+        
+        res = list(set(res))  # remove duplicates
+        res.sort()
+        
+        # Scan for the first missing positive
+        expected = 1
+        for num in res:
+            if num == expected:
+                expected += 1
+            elif num > expected:
+                return expected
+        
+        return expected
