ea62e3d7fc2b674fdafda3f38fb181597ef9ccb8
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-11-14 12:40:26 -0600

    Time: 39 ms (98.85%) | Memory: 19 MB (81.84%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,26 +1,35 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        curr = prev = res = 0
+        curr = prev = res = 0 
         op = '+'
         s += '+'  #add an operator '+' to ensure the last number is processed
 
         for ch in s:
+
             if ch.isdigit():
                 curr = curr * 10 + int(ch)
+
             elif ch in "+-*/":
+                
                 if op == '+':
                     res += prev
-                    prev = curr
+                    prev = curr # prev stores the previous curr for one iteration
+                                # just to mmake sure, if next op is '*' or '/' it'll immediately perform it
+                                # if op is '+'/'-' it'll add/subtract the previous curr to res
+
                 elif op == '-':
                     res += prev
                     prev = -curr
+
                 elif op == '*':
                     prev *= curr
+
                 elif op == '/':
                     prev = int(prev / curr)  # Truncate toward zero
 
                 op = ch
                 curr = 0
+
             elif ch == ' ':
                 continue
 
