6c6f122bc891430c2a9bc18ff5fcb7153faf373a
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-12-02 20:38:22 -0600

    Time: 1897 ms (82.37%) | Memory: 102.6 MB (72.43%) - LeetSync

--- 3897-count-number-of-trapezoids-ii/count-number-of-trapezoids-ii.py
+++ 3897-count-number-of-trapezoids-ii/count-number-of-trapezoids-ii.py
@@ -1,26 +1,36 @@
 class Solution:
     def countTrapezoids(self, points: List[List[int]]) -> int:
-        m = Counter()
+
+        res = 0
+        
+        slopes  = Counter()
         lines = Counter()
-        mdpt = Counter()
-        mdln = Counter()
+        midpoints = Counter()
+        midlines = Counter()
 
-        for (x1, y1), (x2, y2) in combinations(points, 2):
-            dy, dx = y2-y1, x2-x1
-            g = gcd(dy, dx) # to reduce the fraction
-            dy, dx = dy // g, dx // g
-            if dx < 0 or (dx == 0 and dy < 0):
-                dy, dx = -dy, -dx
+        for (x1, y1) , (x2, y2) in combinations(points, 2):
 
+            dy = y2 - y1
+            dx = x2 - x1
+
+            g = gcd(dy, dx)
+
+            dy = dy // g 
+            dx = dx // g
+
+            if (dy, dx) < (0, 0):
+                dy, dx = - dy, - dx
+            
             c = dx * y1 - dy * x1
-            m[dy, dx] += 1
-            lines[dy, dx, c] += 1
-            mdpt[y1+y2, x1+x2] += 1
-            mdln[y1+y2, x1+x2, dy, dx, c] += 1
 
-        res = sum(comb(v, 2) for v in m.values()) # add all same slopes
-        res -= sum(comb(v, 2) for v in lines.values()) # minus all collinear lines
-        res -= sum(comb(v, 2) for v in mdpt.values()) # minus extra lines with same midpoint (paralellograms) (double-counted)
-        res += sum(comb(v, 2) for v in mdln.values()) # add back collinear diagonals (over-subtracted)
+            slopes[dy, dx] += 1
+            lines[dy, dx, c] += 1
+            midpoints[x1 + x2, y1 + y2] += 1
+            midlines[x1 + x2, y1 + y2, dy, dx, c ] += 1
+
+        res += sum(comb(v, 2) for v in slopes.values())
+        res -= sum(comb(v, 2) for v in lines.values())
+        res -= sum(comb(v, 2) for v in midpoints.values())
+        res += sum(comb(v, 2) for v in midlines.values())
 
         return res
