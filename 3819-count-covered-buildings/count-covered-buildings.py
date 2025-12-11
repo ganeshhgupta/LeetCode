4edb3d39d27878be5d09e56f740b1f60cb43e54a
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2025-12-10 23:29:26 -0600

    Time: 740 ms (38.66%) | Memory: 62.7 MB (26.89%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.py
+++ 3819-count-covered-buildings/count-covered-buildings.py
@@ -1,20 +1,28 @@
 class Solution:
     def countCoveredBuildings(self, n: int, b: List[List[int]]) -> int:
-        rmax = [0] * (n + 1)
-        rmin = [n + 1] * (n + 1)
-        cmax = [0] * (n + 1)
-        cmin = [n + 1] * (n + 1)
+      
+        # O(n), O(n)
+        # for every x-coord find min and max y, vice versa
+        
+        res = 0
+
+        x_min = defaultdict(lambda: math.inf)
+        x_max = defaultdict(lambda: -math.inf)
+        y_min = defaultdict(lambda: math.inf)
+        y_max = defaultdict(lambda: -math.inf)
 
         for x, y in b:
-            rmax[y] = max(rmax[y], x)
-            rmin[y] = min(rmin[y], x)
-            cmax[x] = max(cmax[x], y)
-            cmin[x] = min(cmin[x], y)
-
-        ans = 0
-
+            x_min[y] = min(x_min[y], x)
+            y_min[x] = min(y_min[x], y)
+            x_max[y] = max(x_max[y], x)
+            y_max[x] = max(y_max[x], y)
+        
         for x, y in b:
-            if rmin[y] < x < rmax[y] and cmin[x] < y < cmax[x]:
-                ans += 1
+            if y_min[x] < y < y_max[x] and x_min[y] < x < x_max[y]:
+                res += 1
+        
+        return res
 
-        return ans
+    
+
+    
