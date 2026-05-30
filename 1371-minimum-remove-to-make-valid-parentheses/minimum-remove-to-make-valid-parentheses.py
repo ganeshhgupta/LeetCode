53e57f675c4ba256f468727108e9e8114819be3a
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2026-05-30 01:10:49 -0500

    Time: 35 ms (75.20%) | Memory: 22 MB (5.21%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,25 +1,37 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         
+        # O(n), O(n)
+        # two pass, we can always remove the last '('s
+
         res = []
-        c = 0
+        c = 0 # extra ( param
+
+        # first pass to remove extra ):
 
         for i in s:
             if i == '(':
                 c += 1
                 res.append(i)
+            
             elif i == ')' and c > 0:
-                c -= 1
-                res.append(i)
-            elif i != ')' :
+                c -=1
                 res.append(i)
 
-        res[:] = res[::-1]
-        ans = []
+            elif i != ')':
+                res.append(i) # reg chars
+        
+        filtered = []
+        res = reversed(res)
+
+        # second pass to remove extra (:
+
         for i in res:
             if i == '(' and c > 0:
                 c -= 1
             else:
-                ans.append(i)
+                filtered.append(i)
         
-        return "".join(ans[::-1])
+        return "".join(filtered[::-1])
+
+
