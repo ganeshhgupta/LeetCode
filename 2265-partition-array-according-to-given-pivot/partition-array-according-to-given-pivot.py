f35766825c7862732d72470e0885a8bbf0811d95
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2026-06-08 02:21:34 -0500

    Time: 25 ms (78.30%) | Memory: 34.1 MB (10.42%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,35 +1,18 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
         
-        l1 = []
-        l2 = []
-        l3 = []
+        mid = 0
+        left = []
+        right = []
 
-        for i in range(len(nums)):
-            if nums[i] < pivot:
-                l1.append(nums[i])
-            elif nums[i] == pivot:
-                l2.append(nums[i]) 
+        for n in nums:
+            if n < pivot:
+                left.append(n)
+            
+            elif n > pivot:
+                right.append(n)
+            
             else:
-                l3.append(nums[i])
-
-        print(l1, l2, l3)
-        return l1 + l2 + l3
-
-    # 9,12,5,10,14,3,10
-
-    #bubbling left to right:
-
-    #9,5, 12,10,14,3,10
-    #9,5, 10,12,14,3,10
-    #9,5, 10,12,14,3,10
-
-    #9,5, 10,12,3,14,10
-    #9,5, 10,12,3,10,14
-
-    #right to left
-
-    #9,5, 10,12,3,14,10
-    #9,5, 10,12,3,10,14
-     #9,5, 10,3,10,12,14
-    #9,5, 3, 10,10,12,14
+                mid += 1
+        
+        return left + [pivot] * mid + right
