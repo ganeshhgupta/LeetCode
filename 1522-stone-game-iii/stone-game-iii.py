252284f3de5a52bb406eb617f4fa9aa280bdbb08
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2026-08-02 22:04:10 -0500

    Time: 1671 ms (19.49%) | Memory: 169.2 MB (29.87%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+
+        dp = {}
+
+        # Return (a-b), (b-a)
+
+        def dfs(i):
+            if i >= len(stoneValue):
+                return 0
+            if i in dp:
+                return dp[i]
+            
+            res = float("-inf")
+            
+            for j in range(i, i + 3):
+                res = max(res, sum(stoneValue[i : j+1]) - dfs(j + 1))
+            
+            dp[i] = res
+            return res
+        
+        return "Alice" if dfs(0) > 0 else ("Bob" if dfs(0) < 0 else "Tie")
