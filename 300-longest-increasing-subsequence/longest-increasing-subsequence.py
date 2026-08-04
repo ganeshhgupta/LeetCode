71b89d0293fffa0c3ce6adabf0ae124ec7a9e736
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2026-08-04 04:09:37 -0500

    Time: 11 ms (78.03%) | Memory: 19.6 MB (16.47%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,22 +1,29 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
+        # O(n log n), O(len(LIS) = n)
+
+        # loop across main array, initial LIS = first element of main array
+        # for every value n, do bisect_left to see where can you insert it into the LIS
+        # If it's larger than all elements, append it (len(LIS) increases), else, replace the first element >= it
+
         LIS = [nums[0]]
 
         for n in nums:
 
-            l, r = 0, len(LIS)
-            while l < r:
-                m = l + (r - l) // 2
-
-                if LIS[m] < n:
-                    l = m + 1
-                else:
-                    r = m
+            left, right = 0, len(LIS)
             
-            if l == len(LIS):
+            while left < right:
+                mid = left + (right - left) // 2
+                
+                if LIS[mid] < n:
+                    left = mid + 1
+                else:
+                    right = mid
+        
+            if left == len(LIS):
                 LIS.append(n)
             else:
-                LIS[l] = n
+                LIS[left] = n
         
         return len(LIS)
