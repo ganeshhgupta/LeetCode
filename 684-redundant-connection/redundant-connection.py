88d2b9e188b5f81eae8700859614c7d541154cfd
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2026-08-04 21:24:45 -0500

    Time: 3 ms (61.32%) | Memory: 19.7 MB (28.22%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,31 +1,32 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         
-        #union-find, union by rank, path-compression
-        #list comprehension - [0, 1, 2, ...] #every component is their own parent
-        n = len(edges)
-        par = [i for i in range(n+1)]
-        rank = [1] * (n + 1) #size of each component
+        # O(N²), O(N)
+        # to get [0, 1, .. n] 0th index stays unsed in both arrays, since input says 1-n
 
+        n = len(edges)
+        par = [i for i in range(n + 1)]
+        rank = [1] * (n + 1)
+    
         def find(n):
             if n != par[n]:
                 par[n] = find(par[n])
             return par[n]
-
-        def union(n1, n2): #True if n1, n2 are disconnected components
+        
+        def union(n1, n2):
             p1, p2 = find(n1), find(n2)
+
             if p1 == p2:
                 return False
-
+            
             if rank[p1] > rank[p2]:
                 par[p2] = p1
-                rank[p1] += rank[p2]
+                rank[p2] += rank[p1]
             else:
                 par[p1] = p2
-                rank[p2] += rank[p2]
-
+                rank[p1] += rank[p2]  
             return True
-
+        
         for n1, n2 in edges:
-            if not union(n1, n2): #if n1, n2 are connected: 
-                return [n1, n2] #redundant edge
+            if not union(n1, n2):
+                return (n1, n2)
