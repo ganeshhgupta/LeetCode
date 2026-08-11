7aae2e3780661cfe4d00d18f6783294338717aac
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2026-08-11 04:12:18 -0500

    Time: 3 ms (70.24%) | Memory: 21 MB (29.01%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.py
+++ 337-house-robber-iii/house-robber-iii.py
@@ -6,19 +6,24 @@
 #         self.right = right
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-
+        
+        # O(n), O(1)
+        
         def dfs(n):
+
             if not n:
                 return (0, 0)
-        
+            
             left = dfs(n.left)
             right = dfs(n.right)
 
-            rob = n.val + left[1] + right[1]
-            skip = max(left) + max(right)
+            rob = n.val + left[1] + right[1]    # take curr val 
+                                                # + max val from left subtree if left child val is skipped
+                                                # + max val from right subtree if right child val is skipped
+            skip = max(left) + max(right)       # skip curr val, take max so far frmo left and right subtrees
 
             return (rob, skip)
         
         return max(dfs(root))
 
-        
+            
