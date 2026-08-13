444ae94eeebe9881fdb2046022cab5f06b6aeded
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2026-08-12 21:56:51 -0500

    Time: 111 ms (30.84%) | Memory: 35.3 MB (10.67%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -2,17 +2,17 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         
         # O(n), O(n)
-        
-        res = [0] * len(temperatures)
+
         st = [] # (idx, temp)
+        res = [0] * len(temperatures)
 
-        for i, t in enumerate(temperatures):
+        for i, temp in enumerate(temperatures):
 
-            while st and st[-1][1] < t: # when curr t > st[-1] t : popping happens
-                idx, temp = st.pop()
-                res[idx] = i - idx
-            
-            st.append((i, t))
+            while st and st[-1][1] < temp:
+                st_i, st_temp = st.pop()
+                res[st_i] = i - st_i
 
+            st.append((i, temp))
+        
         return res
-             
+
