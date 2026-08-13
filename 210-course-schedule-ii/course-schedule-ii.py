828d847961c5b2151bee1f2c19bdad098d99eef7
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2026-08-13 06:43:12 -0500

    Time: 7 ms (26.87%) | Memory: 20.4 MB (73.90%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,30 +1,36 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        
+
+        # KAHN's: O(V + E), O(V + E)
+        # Courses with no prerequisites have indegree = 0.
+        # Start BFS with those courses, then decrease indegree of neighbors.
+
+        adj = defaultdict(list)
+        indegree = [0] * numCourses
         res = []
-        #list comp: map course to prerequisites
-        preMap = {i:[] for i in range(numCourses)}
-        for crs, pre in prerequisites:
-            preMap[crs].append(pre)
 
-        visitSet, cycleSet = set(), set()
-        def dfs(crs):
-            if crs in cycleSet:
-                return False
-            if crs in visitSet:
-                return True
+        for c, prereq in prerequisites:
+            adj[prereq].append(c)
+            indegree[c] += 1
 
-            cycleSet.add(crs)
+        q = deque()
 
-            for pre in preMap[crs]:
-                if not dfs(pre): return False
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+                res.append(i)
 
-            cycleSet.remove(crs)
-            visitSet.add(crs)
-            res.append(crs)
-            return True
+        count = 0
 
-        for crs in range(numCourses):
-            if not dfs(crs): return []
-        return res
+        while q:
+            course = q.popleft()
+            count += 1
 
+            for nei in adj[course]:
+                indegree[nei] -= 1
+
+                if indegree[nei] == 0:
+                    q.append(nei)
+                    res.append(nei)
+
+        return res if count == numCourses else []
