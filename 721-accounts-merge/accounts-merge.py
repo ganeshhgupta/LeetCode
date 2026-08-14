3adf9543c280f3f469e435513e1b17f41632fda1
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2026-08-14 05:02:37 -0500

    Time: 19 ms (89.70%) | Memory: 22.3 MB (56.69%) - LeetSync

--- 721-accounts-merge/accounts-merge.py
+++ 721-accounts-merge/accounts-merge.py
@@ -1,26 +1,52 @@
-class Solution:    
+class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        graph = defaultdict(set)
-        for a in accounts:
-            for email in a[1:]:
-                graph[a[1]].add(email)
-                graph[email].add(a[1])
-        #print(graph.items())
 
-        def dfs(node, visit):
-            visit.add(node)
-            for nei in graph[node]:
-                if nei not in visit:
-                    dfs(nei, visit)
-            self.res.append(node)
+        # O(N + E log E) n - acc, E - emails
         
-        visit = set()
-        ans = []
-        for a in accounts:
-            name = a[0]
-            for email in a[1:]:
-                if email not in visit:
-                    self.res = []
-                    dfs(email, visit)
-                    ans.append([name]+sorted(self.res))
-        return ans
+        n = len(accounts)
+        uf = UF(n)
+        email_to_id = {} # {email : index of that entry} 
+                         # if an email appears in multiple entries, it will be unioned with its initially found entry
+
+        for i, account in enumerate(accounts):
+            for email in account[1:]:
+                if email not in email_to_id:
+                    email_to_id[email] = i
+                else:
+                    uf.union(i, email_to_id[email])
+
+        groups = defaultdict(list)
+
+        for email, i in email_to_id.items():
+            groups[uf.find(i)].append(email)
+
+        res = []
+
+        for i, emails in groups.items():
+            res.append([accounts[i][0]] + sorted(emails))
+
+        return res
+
+
+
+class UF:
+    def __init__(self, n):
+        self.par = list(range(n))
+        self.rank = [1] * n
+
+    def find(self, x):
+        if self.par[x] != x:
+            self.par[x] = self.find(self.par[x])
+        return self.par[x]
+
+    def union(self, x, y):
+        x, y = self.find(x), self.find(y)
+
+        if x == y:
+            return
+
+        if self.rank[x] < self.rank[y]:
+            x, y = y, x
+
+        self.par[y] = x
+        self.rank[x] += self.rank[y]
