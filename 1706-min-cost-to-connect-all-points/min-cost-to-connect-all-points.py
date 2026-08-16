a26d932d457c7d847e7aa8b710dda2a9f700b1cb
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2026-08-16 07:28:49 -0500

    Time: 523 ms (81.93%) | Memory: 19.7 MB (87.15%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,52 +1,23 @@
-class UF:
-    def __init__(self, n):
-        self.par = list(range(n))
-        self.rank = [1] * n
-
-    def find(self, x):
-        while x != self.par[x]:
-            self.par[x] = self.par[self.par[x]]
-            x = self.par[x]
-        return x
-
-    def union(self, a, b):
-        a, b = self.find(a), self.find(b)
-
-        if a == b:
-            return False
-
-        if self.rank[a] < self.rank[b]:
-            a, b = b, a
-
-        self.par[b] = a
-        self.rank[a] += self.rank[b]
-        return True
-
-
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
 
         n = len(points)
-        edges = []
-
-        for i in range(n):
-            for j in range(i + 1, n):
-                cost = abs(points[i][0] - points[j][0]) + \
-                       abs(points[i][1] - points[j][1])
-                edges.append((cost, i, j))
-
-        edges.sort()
-
-        uf = UF(n)
+        dist = [float("inf")] * n
+        dist[0] = 0
+        seen = set()
         res = 0
-        count = 0
 
-        for cost, i, j in edges:
-            if uf.union(i, j):
-                res += cost
-                count += 1
+        for _ in range(n):
+            i = min((i for i in range(n) if i not in seen),
+                    key=lambda i: dist[i])
 
-                if count == n - 1:
-                    break
+            seen.add(i)
+            res += dist[i]
+
+            for j in range(n):
+                if j not in seen:
+                    d = abs(points[i][0] - points[j][0]) + \
+                        abs(points[i][1] - points[j][1])
+                    dist[j] = min(dist[j], d)
 
         return res
