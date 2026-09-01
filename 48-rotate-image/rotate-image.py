e1719901ec9b9e5f5e8ab3d9ed2ad79a272b2f87
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2026-09-01 12:12:01 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (28.88%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -1,22 +1,13 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        
-        left, right = 0, len(matrix) - 1
 
-        while left < right:
-            for i in range(right - left):
-                top, bottom = left, right
+        n = len(matrix)
 
-                top_left = matrix[top][left + i] # save top_left to temp
+        # Transpose
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-                matrix[top][left + i] = matrix[bottom - i][left] #bl-tl
-                matrix[bottom - i][left] = matrix[bottom][right - i] #br-bl
-                matrix[bottom][right - i] = matrix[top + i][right] #tr-br
-                matrix[top + i][right] = top_left #tl-tr(temp)
-                
-            left += 1
-            right -= 1
-            
+        # Reverse each row
+        for row in matrix:
+            row.reverse()
