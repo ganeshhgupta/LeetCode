cbc41a5b83f34a344a73fe0e777c118865211413
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2026-09-04 00:22:44 -0500

    Time: 1638 ms (5.17%) | Memory: 375.5 MB (8.71%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,12 +1,23 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
 
-        # O(amount × coins), O(amount)
-        dp = [0] * (amount + 1)
-        dp[0] = 1
+        # O(amount × n), O(amount × n)
+
+        cache = {}
+
+        def dfs(i, curr):
+
+            if curr > amount or i == len(coins):
+                return 0
+            
+            if curr == amount:
+                return 1
+            
+            if (i, curr) in cache:
+                return cache[(i, curr)]
+            
+            cache[(i, curr)] = dfs(i, curr + coins[i] ) + dfs(i + 1, curr)
         
-        for c in coins:
-            for i in range(c, amount + 1):
-                dp[i] += dp[i - c] # only difference being '+=' instead of '='
-        
-        return dp[amount]
+            return cache[(i, curr)]
+    
+        return dfs(0, 0)
