bbc01ecf3c9b03b76f8c0ab08d767952dcc0c313
Ganesh Gupta <53180576+ganeshhgupta@users.noreply.github.com>
2026-09-03 23:09:35 -0500

    Time: 3 ms (29.82%) | Memory: 21.2 MB (98.75%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -2,27 +2,30 @@ class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
 
         # O(n), O(n)
-        
+
         res = []
         i = 0
         n = len(intervals)
 
+        new = newInterval
+        intv = intervals
+
         # Before overlap
-        while i < n and intervals[i][1] < newInterval[0]:
-            res.append(intervals[i])
+        while i < n and intv[i][1] < new[0]:
+            res.append(intv[i])
             i += 1
 
         # Overlap + merge
-        while i < n and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
+        while i < n and intv[i][0] <= new[1]:
+            new[0] = min(new[0], intv[i][0])
+            new[1] = max(new[1], intv[i][1])
             i += 1
 
-        res.append(newInterval)
+        res.append(new)
 
         # After overlap
         while i < n:
-            res.append(intervals[i])
+            res.append(intv[i])
             i += 1
 
         return res
